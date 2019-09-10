4563f992ff7b942b8337ea78b163929ad148e80b
Sebastian Scholz <sebastian.scholz@uni-wuppertal.de>
2019-09-10 18:10:01 +0200

    Update version number to 0.2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,9 +22,9 @@ project = 'pynami'
 copyright = '2019, Sebastian Scholz'
 author = 'Sebastian Scholz'
 
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
