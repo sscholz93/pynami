b0405ee7a1d857c30901d3ac5c25e9e265a762ad
Sebastian Scholz <sebastian.scholz@uni-wuppertal.de>
2019-09-23 23:26:21 +0200

    Minor fix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-from .pynami import __version__
+from pynami import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
